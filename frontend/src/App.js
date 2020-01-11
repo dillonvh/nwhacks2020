a675cd260ab0fd2b234d4b91c97ca754458c1a06
Dillon <dvanhorn@sfu.ca>
2020-01-11 14:38:08 -0800

    We got a button boys

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@material-ui/core/Button";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Main">
+        <Button variant="contained" color="primary" size="large">
+          Start Session <PlayCircleOutlineIcon />
+        </Button>
+      </div>
     </div>
   );
 }
