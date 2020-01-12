b5d657d7c449bac87bf01c1f50e707fdb694adfe
Dillon <dvanhorn@sfu.ca>
2020-01-11 17:43:02 -0800

    Add VisionAPI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,11 @@ import Webcam from "react-webcam";
 import "./App.css";
 import Button from "@material-ui/core/Button";
 import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
-import WebcamCapture from './WebcamCapture';
+import WebcamCapture from "./WebcamCapture";
 import initFirebase from "./Database/Firebase";
+import getVisionAPIResults from "./VisionAPI/Vision";
 
 class App extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -20,28 +20,30 @@ class App extends React.Component {
     const db = initFirebase();
   }
 
-  setImageDataAppState = (imageData) => {
+  setImageDataAppState = imageData => {
     this.setState({
       imageData
     });
   };
 
-  render () {
+  render() {
     return (
       <div className="App">
         <div className="Main">
-          <h1>
-            HackerHelper
-          </h1>
+          <h1>HackerHelper</h1>
           <Button variant="contained" color="primary" size="large">
             Start Session &nbsp;
             <PlayCircleOutlineIcon />
           </Button>
-          <WebcamCapture setImageDataAppState={this.setImageDataAppState}/>
+          <WebcamCapture setImageDataAppState={this.setImageDataAppState} />
         </div>
       </div>
     );
   }
+
+  startSession = () => {
+    // Start the session by writing to firebase
+  };
 }
 
 export default App;
---
+++ frontend/src/VisionAPI/Vision.js
@@ -0,0 +1,18 @@
+const vision = require("@google-cloud/vision");
+
+async function getVisionAPIResults(base64ImageString) {
+  // Creates a client
+  const client = new vision.ImageAnnotatorClient();
+
+  // Performs label detection on the image file
+  const [result] = await client.faceDetection({
+    image: { content: base64ImageString },
+    features: [{ maxResults: 1, type: "FACE_DETECTION" }]
+  });
+
+  const faces = result.faceAnnotations;
+  console.log("Labels:");
+  labels.forEach(label => console.log(label.description));
+}
+
+export default getVisionAPIResults;
